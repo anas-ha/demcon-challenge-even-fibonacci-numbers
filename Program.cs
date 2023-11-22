f1d66684481525f5f342855c7fc94ceec046943b
anas-imh <anas@imhnederland.nl>
2023-11-22 08:23:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Fibbonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 4000000, first = 0, second = 1, sumEven = 0;
+
+            while (true)
+            {
+                int next = first + second;
+
+                if (next > limit) break;
+
+                if (next % 2 == 0)
+                {
+                    sumEven += next;
+                }
+                first = second;
+                second = next;
+            }
+
+            Console.WriteLine(sumEven);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+}
